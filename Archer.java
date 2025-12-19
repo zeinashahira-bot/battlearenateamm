bf13b57927d9493c1a11518b3afe84bb792a6698
yousseffmosaad <youssefmosaad1111@gmail.com>
2025-12-19 22:39:34 +0200

    Delete Archer.java

--- Archer.java
+++
@@ -1,6 +0,0 @@
-import javafx.scene.paint.Color;
-public class Archer extends Shooters {
-    public Archer(String name, int x, int y, Color color) {
-        super(name, 100, x, y, color);
-    }
-}
